d014b8a0bf9f4b0dc2204314d644369e6341feb7
Anders Goncalves da Silva <andersgs@gmail.com>
2021-07-01 13:50:32 -0700

    Add install requires and import version string

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup
+from who2tbp import __version__ as VERSION
 
 setup(
     name='who2tbp',
-    version='0.1.0',
+    version=VERSION,
     packages=['who2tbp'],
     url='github.com/MDU-PHL/who2tbp',
     license='GPLv3',
     author='Anders Goncalves da Silva',
     author_email='andersgs@gmail.com',
     description='A script to convert WHO Excel mutations to TBProfiler database format',
+    install_requires=[
+        'biopython>=1.79',
+        'openpyxl>=3.0.7',
+        'setuptools>=49.6.0',
+        'tqdm>=4.61.1',
+    ],
     entry_points={
         "console_scripts":
             [
